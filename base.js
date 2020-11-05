dc785be0c4dcc010765c82a2e274d311892492ba
姚可鑫 <yaokx@weipaitang.com>
2020-11-05 14:46:07 +0800

    docs: 新增ts md文件

---
+++ base.js
@@ -0,0 +1,10 @@
+var str = 'hello world';
+var arr1 = [];
+arr1.push(1); // 错误
+var abc = 123;
+console.log(abc);
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = 1;
+console.log(sayHello(user));
---
+++ base.ts
@@ -0,0 +1,12 @@
+let str: string = 'hello world'
+let arr1: number[] = [];
+arr1.push(1); // 错误
+
+let abc = 123;
+console.log(abc);
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let user = 1;
+console.log(sayHello(user);
